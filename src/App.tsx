592b2534789db3dcc4703a4150c8d3215c3e7b96
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-04-19 14:50:43 -0600

    ramdom category part 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 // App.tsx
-import { useState } from 'react'; // Solo necesitas importar useState aquí
-
+import { useState } from 'react';
 import Hangman from './components/Hangman';
 import Welcome from './components/Welcome';
 
-const words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grap', 'kiwi'];
+const categories = {
+  fruits: ['apple', 'banana', 'cherry', 'date', 'fig', 'grap', 'kiwi'],
+  animals: ['cat', 'dog', 'elephant', 'giraffe', 'lion', 'tiger', 'zebra'],
+  countries: ['argentina', 'brazil', 'canada', 'denmark', 'egypt', 'france', 'germany'],
+  sports: ['football', 'basketball', 'tennis', 'swimming', 'baseball', 'volleyball', 'rugby', 'golf'],
+  planets: ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
+};
 
 function App() {
   const [showGame, setShowGame] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const handlePlayClick = () => {
     setShowGame(true);
+    const categoriesKeys = Object.keys(categories);
+    const randomCategory = categoriesKeys[Math.floor(Math.random() * categoriesKeys.length)];
+    setSelectedCategory(randomCategory);
   };
 
   const handleBackClick = () => {
     setShowGame(false);
+    setSelectedCategory('');
   };
 
   return (
     <div>
       {showGame ? (
-        <Hangman words={words} onBackClick={handleBackClick} /> // Pasar la función onBackClick
+        <Hangman words={categories[selectedCategory]} category={selectedCategory} onBackClick={handleBackClick} />
       ) : (
         <Welcome onPlayClick={handlePlayClick} />
       )}
@@ -29,3 +39,5 @@ function App() {
 }
 
 export default App;
+
+
